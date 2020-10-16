266331b676730c441ffa1fb63e7c595c1e603e2f
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2020-10-16 10:30:59 +0700

    Initial Commit

---
+++ src/if1/pkg10119033/latihan6/kambingstatic/KambingStatic.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119033.latihan6.kambingstatic;
+
+/**
+ *
+ * @author
+ * NAMA     : Dandi Ahmadin
+ * KELAS    : IF-1
+ * NIM      : 10119033
+ * Deskripsi Program : Program ini berisi deklarasi jenis variabel konstanta dan pemanggilan variabel 
+ * static
+ */
+
+public class KambingStatic {
+
+    public static final String NAMA_KAMBING = "MIDUN";
+
+    public static void main(String[] args) {
+        Mamalia.jumlahKambing = 485000;
+        System.out.println(NAMA_KAMBING + " memiliki kambing sebanyak " + Mamalia.jumlahKambing);
+    }
+}
---
+++ src/if1/pkg10119033/latihan6/kambingstatic/Mamalia.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119033.latihan6.kambingstatic;
+
+/**
+ *
+ * @author
+ * NAMA     : Dandi Ahmadin
+ * KELAS    : IF-1
+ * NIM      : 10119033
+ * Deskripsi Program : Program ini berisi deklarasi jenis variabel kelas/static
+ */
+public class Mamalia {
+    // variabel jumlahKambing dideklarasi sebagai statik
+    public static int jumlahKambing;
+}
